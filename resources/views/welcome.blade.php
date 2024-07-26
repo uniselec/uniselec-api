5b609a15c64d6c8aaad1c7db7ae78a2cb9a9909f
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-07-25 21:43:56 -0300

    teste cicd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -832,21 +832,9 @@
 
 <body class="antialiased">
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="mt-16">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-
-                    <div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">>Sistema de Seleção de Candidatos em Construção</h2>
-
-
-                    </div>
-                </div>
-            </div>
-
+        <div>
+            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Sistema de Seleção de Candidatos em Construção</h2>
         </div>
     </div>
 </body>
-
 </html>
