84003ea7f2efab605ebd3b5bac745ba8717071a8
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-08-03 19:52:53 -0300

    Reset Password

--- app/Http/Controllers/Api/Auth/PasswordResetController.php
+++ app/Http/Controllers/Api/Auth/PasswordResetController.php
@@ -14,12 +14,17 @@ class PasswordResetController extends Controller
 {
     public function forgot(Request $request)
     {
-        $request->validate(['email' => 'required|string']);
+        $request->validate(['cpf' => 'required|string']);
+        $user = User::where("cpf", $request->cpf)->first();
+        if(!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
         $status = Password::sendResetLink(
-            $request->only('email')
+            ['email' => $user->email]
         );
         return $status === Password::RESET_LINK_SENT
-            ? response()->json(['message' => __($status)])
+            ? response()->json(['message' => __($status), 'email' => $user->email])
             : response()->json(['email' => __($status)], 422);
     }
     public function reset(Request $request)
