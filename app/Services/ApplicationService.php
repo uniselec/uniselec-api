e6158cebd6aff770d54a9281fff8a7118a818b7d
Milton Cavalcante <milton.cneto@gmail.com>
2025-10-14 12:30:24 -0300

    Remove adjustment of inconsistency reasons based on selected source for name, CPF, and birthdate

--- app/Services/ApplicationService.php
+++ app/Services/ApplicationService.php
@@ -6,8 +6,6 @@ use App\Http\Resources\ApplicationOutcomeResource;
 use Illuminate\Validation\ValidationException;
 use App\Http\Resources\ApplicationResource;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
-use App\Enums\InconsistencyType;
 use Illuminate\Http\Request;
 use App\Models\Application;
 
@@ -27,47 +25,15 @@ class ApplicationService
             throw new ValidationException($validator);
         }
 
-        $application = Application::findOrFail($id);
-        $applicationOutcome = $application->applicationOutcome;
         $sources = $request->only(['name_source', 'birthdate_source', 'cpf_source']);
+        $application = Application::findOrFail($id);
 
-        if($applicationOutcome->reason) {
-            $reasons = array_map('trim', explode(';', $applicationOutcome->reason));
-        } else {
-            $reasons = [];
-        }
-
-        foreach ($sources as $field => $source) {
-            if ($source && in_array($source, ['enem', 'application'])) {
-               
-                $enum = match ($field) {
-                    'name_source'      => InconsistencyType::NAME,
-                    'birthdate_source' => InconsistencyType::BIRTHDATE,
-                    'cpf_source'       => InconsistencyType::CPF,
-                };
-
-                $reasonIndex = array_search($enum->value, $reasons);
-
-                if ($reasonIndex !== false) {
-                    unset($reasons[$reasonIndex]);
-                }
-            }
-        }
-
-        try {
-            DB::beginTransaction();
-            
+        try {           
             $application->update($sources);
-
-            $reasonsString = empty($reasons) ? null : implode(';', array_values($reasons));
-            $applicationOutcome->update(['reason' => $reasonsString]);
-
-            DB::commit();
         } catch (\Exception $e) {
-            DB::rollBack();
             return ServiceResult::withStatus('failure', [], 'Erro interno ao resolver inconsistências', 500);
         }
 
-        return ServiceResult::withStatus('success', ['application' => new ApplicationResource($application->fresh()), 'applicationOutcome' => new ApplicationOutcomeResource($applicationOutcome->fresh())], 'Pendências resolvidas com sucesso', 200);
+        return ServiceResult::withStatus('success', ['application' => new ApplicationResource($application->fresh()), 'applicationOutcome' => new ApplicationOutcomeResource($application->applicationOutcome)], 'Pendências resolvidas com sucesso', 200);
     }
 }
