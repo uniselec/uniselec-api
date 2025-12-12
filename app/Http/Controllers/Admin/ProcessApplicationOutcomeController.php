bddd37e057b0ee87488457e61f3025688e6fc83c
Jefferson Uchoa Ponte <jefponte@gmail.com>
2025-12-12 11:00:14 -0300

    Update timeout

--- app/Http/Controllers/Admin/ProcessApplicationOutcomeController.php
+++ app/Http/Controllers/Admin/ProcessApplicationOutcomeController.php
@@ -11,9 +11,9 @@ class ProcessApplicationOutcomeController extends Controller
     public function processOutcomes(int $selectionId)
     {
 
-        ini_set('memory_limit', '2048M');
-        set_time_limit(0);
-        ini_set('max_execution_time', '0');
+        @ini_set('memory_limit', '1024M');
+        @ini_set('max_execution_time', '0');
+        @set_time_limit(0);
         (new ProcessApplicationOutcome($selectionId))->process();
 
         return response()->json([
